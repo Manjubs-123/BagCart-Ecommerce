16d79f325d54b428e932874af9192ab2e9d61f0a
Manju B S <manjubs86087@gmail.com>
2025-11-19 12:06:53 +0530

    user side address add edit delete successfully completed

--- models/userModel.js
+++ models/userModel.js
@@ -1,16 +1,24 @@
 import mongoose from "mongoose";
 
 const addressSchema = new mongoose.Schema({
+    
+    addressType: { type: String, default: "home" },
+
     fullName: { type: String, required: true, trim: true },
     phone: { type: String, required: true },
-    pincode: { type: String, required: true },
-    house: { type: String, required: true },
-    area: { type: String, required: true },
-    landmark: { type: String },
+
+    addressLine1: { type: String, required: true },   // House + Area combined
+    addressLine2: { type: String },                   // Landmark / Optional
+
     city: { type: String, required: true },
     state: { type: String, required: true },
+
+    pincode: { type: String, required: true },
+    country: { type: String, required: true, default: "India" },
+
     isDefault: { type: Boolean, default: false },
 }, { _id: true });
+
 const userSchema = new mongoose.Schema({
 
     name: { type: String, required: true, trim: true },
